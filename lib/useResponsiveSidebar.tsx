784c324c8396f8c8e06f0b6530313886ead59a09
Kevin Thompson <kevin@theautomaters.com>
2025-12-16 18:45:32 -0800

    Update styling to support mobile better

---
+++ lib/useResponsiveSidebar.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export function useResponsiveSidebar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      const mobile = window.innerWidth < 768;
+      setIsMobile(mobile);
+      if (!mobile && !showSidebar) {
+        setShowSidebar(true);
+      }
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, [showSidebar]);
+
+  return { showSidebar, setShowSidebar, isMobile };
+}
